2b40608818dc320416030d0361cae464de802ef7
burak <75986477+brkkartaloglu@users.noreply.github.com>
2022-08-20 17:17:39 +0300

    Add files via upload

--- day_16_coffee_machine_OOP/main.py
+++ day_16_coffee_machine_OOP/main.py
@@ -1,1 +1,20 @@
+from menu import Menu
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
 
+power = "on"
+menu = Menu()
+machine = CoffeeMaker()
+moneyprocces = MoneyMachine()
+while power == "on":
+    order_name = input(f"What would you like? {menu.get_items()}: ")
+    if order_name == "off":
+        power = "off"
+    elif order_name == "report":
+        machine.report()
+        moneyprocces.report()
+    else:
+        item = menu.find_drink(order_name)
+        if machine.is_resource_sufficient(item):
+            if moneyprocces.make_payment(item.cost):
+                machine.make_coffee(item)
